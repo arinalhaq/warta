57268fe7bd1af0fa90ad097d701cd3f2b1317440
Arinal Haq <ananghaq99@gmail.com>
2019-05-23 19:25:29 +0700

    crud backend

---
+++ backend/views/post/_search.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\PostSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id_post') ?>
+
+    <?= $form->field($model, 'created_by') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'content') ?>
+
+    <?= $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'id_category') ?>
+
+    <?php // echo $form->field($model, 'id_location') ?>
+
+    <?php // echo $form->field($model, 'updated_by') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
